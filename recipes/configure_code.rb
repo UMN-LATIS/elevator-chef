a2dcda89691e1ddaa2bc87c1df69131336d0c2fc
Colin McFadden <cmcfadden@gmail.com>
2016-03-08 18:26:56 -0600

    add email configuration

--- recipes/configure_code.rb
+++ recipes/configure_code.rb
@@ -5,6 +5,16 @@ template "#{node['elevator']['install_directory']}/application/config/config.php
 	source "config.php.erb"
 end
 
+email = data_bag_item("elevator", "emailuser")
+
+template "#{node['elevator']['install_directory']}/application/config/email.php" do
+	source "email.php.erb"
+	variables ({
+		:emailuser => email['username'],
+		:emailpass => email['password']
+	})
+end
+
 databag = data_bag_item("elevator", "dbuser")
 
 template "#{node['elevator']['install_directory']}/application/config/database.php" do
