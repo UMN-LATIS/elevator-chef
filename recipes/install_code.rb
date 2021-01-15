f36a63c68436830943daf3f0cee67508e876bc8c
Colin McFadden <cmcfadden@gmail.com>
2021-01-15 09:48:24 -0600

    try it here

--- recipes/install_code.rb
+++ recipes/install_code.rb
@@ -3,7 +3,7 @@ include_recipe "#{cookbook_name}::git_setup"
 
 node.default['php']['directives'] =  { 'session.gc_maxlifetime' => 43200 }
 
-node['php']['directives'] = {
+override['php']['directives'] = {
   :max_input_vars => '2000'
 }
 
